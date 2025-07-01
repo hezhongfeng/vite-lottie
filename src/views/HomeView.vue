466fb36c5ee8e3cbf907dc7c1b128c2b3ef7c6ad
hezhongfeng <287645587@qq.com>
2025-07-01 14:48:39 +0800

    feat: 初始化Vue3项目基础结构
    添加Vue3项目基础文件结构，包括路由配置、状态管理、组件和样式文件
    配置Vite构建工具、ESLint和Prettier代码规范
    添加Lottie动画支持及相关示例文件

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <DotLottieVue style="height: 500px; width: 500px" playOnHover loop :src="lottie" />
+</template>
+
+<script setup>
+import { DotLottieVue } from '@lottiefiles/dotlottie-vue'
+// import lottie from '@/views/1702973982722.lottie'
+import lottie from '@/views/Ax2k12jKRd.lottie'
+
+// https://lottie.host/16b69e12-0efb-4061-b33d-12dc2b93fd84/Ax2k12jKRd.lottie
+// https://lottie.host/ac861fbc-9341-4dec-a524-654d1cbc6a05/AmqsJl15NM.lottie
+</script>
